59ffb8b3e58ba60349a7e8641ed0e8c44d34ec3b
Christian Moya <cmoyacharro@gamil.com>
2022-06-27 20:12:56 -0500

    taller 15 ejercicio con jdbc

--- src/main/java/com/uce/edu/demo/ProyectoU2CmApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2CmApplication.java
@@ -3,9 +3,13 @@ package com.uce.edu.demo;
 
 
 import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.uce.edu.demo.service.IPersonaJdbcService;
+import com.uce.edu.demo.to.Persona;
 
 
 
@@ -13,6 +17,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProyectoU2CmApplication implements CommandLineRunner {
 	
 	private static Logger logger = Logger.getLogger(ProyectoU2CmApplication.class);
+	
+	@Autowired
+	private IPersonaJdbcService iPersonaJdbcService;
 
 	public static void main(String[] args)  {
 		SpringApplication.run(ProyectoU2CmApplication.class, args);
@@ -23,18 +30,30 @@ public class ProyectoU2CmApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
+		//Insetar
+		Persona persona = new Persona();
+		persona.setApellido("asdasd");
+		persona.setNombre("aaaaaaaaaaaa");
+		persona.setId(5);
 		
-		//debug< mensajes de depuracion
-		//info;mensajes informativos
-		//warn; alartar eventos que se quiren dejar constaancia, pero mno afecta el duncionamiento de la aplicacion
-		//error; alrta evtos que afectan al prrograma, pero no dejan de funcionar 	
-		//fatal errores criticos , despues de guardar el mensake el porgma termina
-		System.out.println("hola mundo");
-		logger.debug("LOG debug");
-		logger.info("LOG info");
-		logger.warn("LOG warn");
-		logger.error("LOG error");
-		logger.fatal("LOG FATAL");
+		//this.iPersonaJdbcService.guardar(persona);
+		
+		
+		Persona per1 = new Persona();
+		per1.setId(1);
+		per1.setNombre("Maria");
+		per1.setApellido("Moya");
+		
+		//Actualizar
+		//this.iPersonaJdbcService.actualizar(per1);
+		
+		//Eliminar
+		//this.iPersonaJdbcService.eliminara(5);
+		
+		//Buscar
+		logger.error(this.iPersonaJdbcService.buscar(3).toString()); 
+		
+		
 	}
 		
 }
